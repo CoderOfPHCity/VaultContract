2beb472b9198cce54fc51be8c7db3f031395e606
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2024-02-23 19:26:37 +0100

    Update Vault.sol

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -2,34 +2,31 @@
 pragma solidity ^0.8;
 
 contract Vault {
+
     error ONLY_OWNER();
-    error ZERO_ADDRESS();
 
     mapping(address => mapping(address => uint256)) balances;
-    uint256 duration = 1 minutes;
-    address owner;
+    uint256 public duration = 1 minutes;
+    address public owner;
 
     function donate() external payable {
         require(msg.value > 0, "Amount must be positive");
-        if (msg.sender == address(0)) {
-            revert ZERO_ADDRESS();
-        }
+        require(msg.sender != address(0), "invalid!");
         balances[msg.sender][address(this)] += msg.value;
     }
 
     function onlyOwner() private view {
-        if (owner != msg.sender) {
+        if (owner == msg.sender) {
             revert ONLY_OWNER();
         }
     }
 
     function addBeneficiary(address _beneficiary) external {
         onlyOwner();
-        if (_beneficiary == address(0)) {
-            revert ZERO_ADDRESS();
-        }
+        require(_beneficiary != address(0), "Invalid beneficiary");
         balances[_beneficiary][address(this)] = 0;
     }
+
 
     function checkBalance(address _beneficiary)
         external
@@ -40,6 +37,7 @@ contract Vault {
     }
 
     function claim() external {
+        require(msg.sender != address(0), "invalid!");
         require(block.timestamp >= duration, "not yet time");
         uint256 amount = balances[msg.sender][address(this)];
         require(amount > 0, "No bal to claim");
